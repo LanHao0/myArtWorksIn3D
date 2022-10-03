769f6843d903f96974d7a72d86b90909c8700a68
LanHao <a913538526@gmail.com>
2022-10-04 02:24:26 +0800

    build: git pages部署相关

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/myArtWorksIn3D/" : "/",
 });
